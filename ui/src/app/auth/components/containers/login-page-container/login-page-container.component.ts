e8e92fc461c37d430018440d064b87c848e2c06b
Topala Ion <topala_ion@yahoo.com>
2022-09-23 12:02:48 +0300

    login page

--- ui/src/app/auth/components/containers/login-page-container/login-page-container.component.ts
+++ ui/src/app/auth/components/containers/login-page-container/login-page-container.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoginPageStore} from "./state/login-page.store";
 import {LoginPageQuery} from "./state/login-page.query";
 import {LoginPageService} from "./state/login-page.service";
+import {ApiErrorModel} from "../../../../../models/error.models";
+import {Observable} from "rxjs";
 
 @Component({
-  selector: 'app-login-page-container',
-  templateUrl: './login-page-container.component.html',
-  styleUrls: ['./login-page-container.component.css'],
+  template: `
+    <app-login-page
+      [loading]="loading$ | async"
+      [error]="error$ | async"
+    ></app-login-page>`,
   providers: [LoginPageStore, LoginPageQuery, LoginPageService]
 })
 export class LoginPageContainerComponent implements OnInit {
 
-  constructor() { }
+  public loading$: Observable<boolean> = this.query.selectLoading();
+  public error$: Observable<ApiErrorModel> = this.query.selectError<ApiErrorModel>();
+
+  constructor(
+    private query: LoginPageQuery
+  ) {
+  }
 
   ngOnInit(): void {
   }
--- ui/src/app/auth/presentational/login-page/login-page.component.ts
+++ ui/src/app/auth/presentational/login-page/login-page.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ApiErrorModel} from "../../../../models/error.models";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnChanges {
+
+  @Input()
+  public loading: boolean = false;
+
+  @Input()
+  public error: ApiErrorModel | null = null;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
   }
 
 }
