12859d2ef1967e6210e5cd8b1c07600689c10d88
Topala Ion <apostion30@gmail.com>
2022-10-26 23:02:33 +0300

    adjustments

--- ui/src/app/dashboard/components/presentational/slider-cards/slider-cards.component.ts
+++ ui/src/app/dashboard/components/presentational/slider-cards/slider-cards.component.ts
@@ -10,7 +10,6 @@ export class SliderCardsComponent implements OnInit {
 
   @Input()
   public movies:MovieResultModel[]
-
   public arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   public totalCards: number = this.arr.length;
   public currentPage: number = 1;
