cb76e7d39811ca7da298ab920c344db571045606
ionursaqi <iursaqi@gmail.com>
2022-10-28 13:38:47 +0300

    validare-1

--- ui/src/app/auth/presentational/register-page/register-page.component.ts
+++ ui/src/app/auth/presentational/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ApiErrorModel} from "../../../../models/error.models";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-page',
@@ -14,13 +14,13 @@ export class RegisterPageComponent implements OnInit {
     email: new FormControl(''),
     password: new FormControl(''),
     age: new FormControl(''),
+    gender: new FormControl(''),
   })
+  private heroForm: FormGroup<{ name: FormControl<any>; power: FormControl<any>; alterEgo: FormControl<any> }>;
   createProfile() {
     this.profileForm.patchValue({
     });
   }
-
-  ;
   onSubmit() {
     console.warn(this.profileForm.value);
   }
@@ -29,10 +29,24 @@ export class RegisterPageComponent implements OnInit {
 
   @Input()
   public error: ApiErrorModel | null;
+  hero: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.heroForm = new FormGroup({
+      name: new FormControl(this.hero.name, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      alterEgo: new FormControl(this.hero.alterEgo),
+      power: new FormControl(this.hero.power, Validators.required)
+    });
+
   }
+
+  get name() { return this.heroForm.get('name'); }
+
+  get power() { return this.heroForm.get('power'); }
 
 }
