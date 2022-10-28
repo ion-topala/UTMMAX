56902159299eba88c72e13319e4589c108102199
Topala Ion <apostion30@gmail.com>
2022-10-28 14:06:23 +0300

    fix auth pages

--- ui/src/app/auth/presentational/register-page/register-page.component.ts
+++ ui/src/app/auth/presentational/register-page/register-page.component.ts
@@ -13,6 +13,7 @@ export class RegisterPageComponent implements OnInit {
     lastName: new FormControl(''),
     email: new FormControl(''),
     password: new FormControl(''),
+    passwordRepeat: new FormControl(''),
     age: new FormControl(''),
     gender: new FormControl(''),
   })
@@ -29,24 +30,9 @@ export class RegisterPageComponent implements OnInit {
 
   @Input()
   public error: ApiErrorModel | null;
-  hero: any;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.heroForm = new FormGroup({
-      name: new FormControl(this.hero.name, [
-        Validators.required,
-        Validators.minLength(4),
-      ]),
-      alterEgo: new FormControl(this.hero.alterEgo),
-      power: new FormControl(this.hero.power, Validators.required)
-    });
-
   }
-
-  get name() { return this.heroForm.get('name'); }
-
-  get power() { return this.heroForm.get('power'); }
-
 }
